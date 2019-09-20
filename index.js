a6154b602c306c06a10727ebb5722b31eb3eb931
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-09-20 14:48:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // Code your solution here:
 const driversWithRevenueOver = (drivers, rev) => {
+  //returns an arr of all matching drivers
+  //returns an empty array if there is no match
   
 };
 
 const driverNamesWithRevenueOver = (drivers, rev) => {
+  //returns an array of all matching drivers names as String
+  //returns an empty array if there is no match
   
 };
 
 const exactMatch = (prop) => {
+  //returns an array of all matching drivers
+  //returns an empty array if there is no match
   
 };
 
 const exactMatchToList = () => {
-  
+  //returns an array of names for all matching drivers
+  //returns an empty array if there is no match
 };
